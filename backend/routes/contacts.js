cd5f7bbeb6a592a43c8eaeb7ea6bfdc9987a245c
Rajat Choudhary <rajatch133@gmail.com>
2023-05-05 16:32:11 +0530

    Backend completed created all endpoints

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -52,4 +52,64 @@ router.post('/addcontact', fetchuser, [
 
 })
 
+//ROUTE 3: Update an existing contact using: PUT "/api/contacts/updatecontact" Login Required
+
+router.put('/updatecontact/:id', fetchuser, async (req, res) => {
+    const { name, email, tag } = req.body;
+    try {
+
+        //create a newContact object
+
+        const newContact = {};
+        if (name) { newContact.name = name };
+        if (email) { newContact.email = email };
+        if (tag) { newContact.tag = tag };
+
+        // find the user who is updating  // param mean id from the url /updatecontact/:id
+        let contact = await Contacts.findById(req.params.id);
+        if (!contact) {
+            return res.status(404).send("Not Found")
+        }
+        //  find the note to be updated and update it
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        // update the the contact with new values, and new true means if there is new contact values then those will be added
+        contact = await Contacts.findByIdAndUpdate(req.params.id, { $set: newContact }, { new: true })
+        res.json({ contact });
+        // const note = Notes.findByIdAndUpdate()
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+//ROUTE 4: Delete an existing contact using: DELETE "/api/contacts/deletecontact" Login Required
+
+router.delete('/deletecontact/:id', fetchuser, async (req, res) => {
+
+    try {
+
+        // find the note to be deleted and delete it
+        let contact = await Contacts.findById(req.params.id);
+        if (!contact) {
+            return res.status(404).send("Not Found")
+        }
+        //  Allow Deletion only if user owns this note
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        contact = await Contacts.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Contact has been deleted", contact: contact });
+        // const note = Notes.findByIdAndUpdate()
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error")
+    }
+})
+
 module.exports = router
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='flex'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
